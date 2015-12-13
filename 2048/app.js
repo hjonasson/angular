3b8512037ed9340274917ff7369a6bc0009764a1
Hjortur Jonasson <hjorturjonasson@gmail.com>
2015-12-13 16:16:14 +0100

    Iterating correctly

--- 2048/app.js
+++ 2048/app.js
@@ -1,4 +1,6 @@
 angular.module('2048app',[])
-	.controller('2048',function () {
-		console.log('Hello world')
+	.controller('2048',function ($scope) {
+		var size = 4;
+		$scope.tiles = Array.apply(null, Array(size * size)).map(Number.prototype.valueOf,NaN);
+		console.log($scope.tiles);
 	});
